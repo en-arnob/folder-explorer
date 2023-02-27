698df78ff3c61eaf80df9468a5fe020273617b47
Khalid Arnob <en.arnob@gmail.com>
2023-02-27 16:54:02 +0600

    Database Connect

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("dotenv").config();
+const connect = require("./config/db");
 
 const app = express();
 
+//db con
+connect();
 
 app.get("/", (req, res) => {
     res.json({
